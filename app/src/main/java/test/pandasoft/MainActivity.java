b5aa5cc0a8ee316387cb6bed231b0f5f7533be6c
tar <jirawat.tar@g.swu.ac.th>
2020-02-15 19:34:55 +0700

    Create single page

--- app/src/main/java/test/pandasoft/MainActivity.java
+++ app/src/main/java/test/pandasoft/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_news);
+        setContentView(R.layout.activity_single_new);
     }
 }
