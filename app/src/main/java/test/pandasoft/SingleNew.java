0ac396f57920a595e9226001b16b09afdd9d0053
tar <jirawat.tar@g.swu.ac.th>
2020-02-15 13:14:11 +0700

    Initial commit

---
+++ app/src/main/java/test/pandasoft/SingleNew.java
@@ -0,0 +1,14 @@
+package test.pandasoft;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SingleNew extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_new);
+    }
+}
